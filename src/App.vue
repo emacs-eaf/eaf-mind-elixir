f8fae4aefe6856a9c8ec3c045945113ab6c085ad
Andy Stewart <lazycat.manatee@gmail.com>
2025-02-28 01:01:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+ import MindElixir from 'mind-elixir'
+
+ export default {
+   name: 'App',
+   components: {
+   },
+   mounted() {
+     let options = {
+       el: '#map', // or HTMLDivElement
+     }
+
+     let mind = new MindElixir(options)
+
+     // create new map data
+     const data = MindElixir.new('new topic')
+     mind.init(data)
+   }
+ }
+</script>
+
+<style>
+ #map {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   width: 100%;
+   height: 100%;
+ }
+
+ html, body {
+   width: 100%;
+   height: 100%;
+   margin: 0;
+   padding: 0;
+ }
+</style>
